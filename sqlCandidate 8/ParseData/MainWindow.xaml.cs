9285075c9b71889b302d8912813a1c71b22f12fa
sssrohit <sssrohitbhongale@gmail.com>
2017-06-21 16:58:53 +0530

    added multiple file select button

--- sqlCandidate 8/ParseData/MainWindow.xaml.cs
+++ sqlCandidate 8/ParseData/MainWindow.xaml.cs
@@ -44,23 +44,6 @@ namespace ParseData
             openFileDialog1.InitialDirectory = @"F:\Rohit";
             openFileDialog1.Title = "Browse PDF Files";
 
-            openFileDialog1.CheckFileExists = true;
-            openFileDialog1.CheckPathExists = true;
-
-            //openFileDialog1.DefaultExt = "txt";
-            //openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
-            //openFileDialog1.FilterIndex = 2;
-            openFileDialog1.RestoreDirectory = true;
-
-            openFileDialog1.ReadOnlyChecked = true;
-            openFileDialog1.ShowReadOnly = true;
-
-            //if (openFileDialog1.ShowDialog() == DialogResult.OK)
-            //if (openFileDialog1.ShowDialog() == DialogResult)
-            //{
-            //    selectfileTB.Text = openFileDialog1.FileName;
-            //}
-
             Nullable<bool> result = openFileDialog1.ShowDialog();
             if (result == true)
             {
@@ -68,6 +51,24 @@ namespace ParseData
                 string filename = openFileDialog1.FileName;
                 selectfileTB.Text = filename;
             }
+        }
+
+        private void multipleBtn_Click(object sender, RoutedEventArgs e)
+        {
+           OpenFileDialog x = new OpenFileDialog();
+           x.Multiselect = true;
+           var result = x.FileNames;
+           x.ShowDialog();
+
+           foreach (string fileName in x.FileNames)
+           {
+               selectfileTB.Text = fileName;
+           }
+        }
+
+        private void exitBtn_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
         }
 
         private void MenuItem_Click_1(object sender, RoutedEventArgs e)
@@ -78,30 +79,6 @@ namespace ParseData
         private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-        }
-
-        private void multipleBtn_Click(object sender, RoutedEventArgs e)
-        {
-            OpenFileDialog x = new OpenFileDialog();
-            x.Multiselect = true;
-           // x.ShowDialog();
-            string[] result = x.FileNames;
-
-            //OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            
-            //Nullable<bool> result1 = openFileDialog1.ShowDialog();
-            Nullable<bool> result1 = x.ShowDialog();
-            if (result1 == true)
-            {
-                // Open document
-
-                string filename = x.FileName;
-                selectfileTB.Text = filename;
-            }
-
-            //foreach (string y in result)
-            //    MessageBox.Show(y, "Selected Item");
-            //, MessageBoxButtons.OK, MessageBoxIcon.Information
         }
     }
 }
